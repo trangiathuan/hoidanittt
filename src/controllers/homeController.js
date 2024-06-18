eab99fab5097a573c9b17d2ea6da22b303ffae7f
trangiathuan <nakrothfaker@gmail.com>
2024-06-19 01:05:29 +0700

    commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,15 +1,9 @@
 const connection = require('../config/database')
 const User = require('../models/user')
 
-
+// Chuyển hướng sang trang thêm user
 const getCreateUser = (req, res) => {
     return res.render('Home.ejs')
-}
-// Lấy danh sách user trong DB
-const getListUser = async (req, res) => {
-    // Lấy tất cả document trong User
-    let results = await User.find({})
-    return res.render('ListUser.ejs', { listUser: results })
 }
 // Thêm user vào DB
 const postCreateUser = async (req, res) => {
@@ -23,11 +17,38 @@ const postCreateUser = async (req, res) => {
         name: name,
         city: city
     })
-    return res.send('Create Success')
+    return res.redirect('/')
 }
+// Lấy đối tượng cần update điền vào phần Edit
+const getUpdateUser = async (req, res) => {
+    const id = req.params.id;
+    //Lấy đối tượng theo id
+    let results = await User.findOne({ _id: id })
+    return res.render('Edit.ejs', { User: results })
+}
+// Update user
+const postUpdateUser = async (req, res) => {
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+    let id = req.params.id;
+    // Câu lện update 1 đối tượng theo id của DB
+    await User.updateOne({ _id: id }, { email: email, name: name, city: city })
+    return res.redirect('/')
+}
+
+// Lấy danh sách user trong DB
+const getListUser = async (req, res) => {
+    // Lấy tất cả document trong User
+    let results = await User.find({})
+    return res.render('ListUser.ejs', { listUser: results })
+}
+
 // Xuất các hàm để nơi khác có thể sử dụng
 module.exports = {
     getCreateUser,
     getListUser,
-    postCreateUser
+    postCreateUser,
+    getUpdateUser,
+    postUpdateUser
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
 // Gọi các hàm ở trong controller để router có thể sử dụng
-const { getCreateUser, getListUser, postCreateUser } = require('../controllers/homeController')
+const { getCreateUser, getListUser, postCreateUser, getUpdateUser, postUpdateUser } = require('../controllers/homeController')
 
 router.get('/', getListUser)
+router.get('/update/:id', getUpdateUser)
 
 router.get('/create-user', getCreateUser)
-// Tạo route và gọi hàm ở controller => gửi yêu cầu cho controller xử lý
+// Tạo route và gọi hàm ở controller => gửi yêu cầu cho controller xử lý yêu cầu create User
 router.post('/createUser', postCreateUser)
+// Tạo route và gọi hàm ở controller => gửi yêu cầu cho controller xử lý yêu cầu update User
+router.post('/updateUser/:id', postUpdateUser)
 
 module.exports = router
