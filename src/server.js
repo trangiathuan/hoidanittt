aaff505f434c2fd5a9768947c894d8fbee587a6b
trangiathuan <nakrothfaker@gmail.com>
2024-06-21 05:19:59 +0700

    commit

--- src/server.js
+++ src/server.js
@@ -1,29 +1,24 @@
 const express = require('express') //import express
-require('dotenv').config();
-const path = require('path')
 const app = express() // tạo express application
 const confiViewEngine = require('./config/viewEngine')
-const port = process.env.PORT || 8888 // init port
+require('dotenv').config();
 const connection = require('./config/database')
 const fileUpload = require('express-fileupload');
-
+const webRoutes = require('./routes/web')
+const apiRoutes = require('./routes/api')
+const port = process.env.PORT || 8888
 
 // default options
 app.use(fileUpload());
-const webRoutes = require('./routes/web')
-const apiRoutes = require('./routes/api')
 
 confiViewEngine(app);
-
+// Convert data to json
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
 //khai báo routes
 app.use(webRoutes)
 app.use('/v1/api', apiRoutes)
-//req (request), res(response) là 2 object trong môi trường Node.js
-
-
-
 
 const conn = async () => {
     try {
